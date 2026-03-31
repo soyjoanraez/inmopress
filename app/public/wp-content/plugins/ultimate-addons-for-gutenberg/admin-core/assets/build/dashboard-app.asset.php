34dc0d4a7a8ddeebbcd21dc67f62a1d2a093cddb
Joan Ráez <161605511+soyjoanraez@users.noreply.github.com>
2026-03-31 10:23:54 +0200

    Delete app directory

--- app/public/wp-content/plugins/ultimate-addons-for-gutenberg/admin-core/assets/build/dashboard-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => 'e01bf53da588432b7e45');
--- app/public/wp-content/themes/inmopress/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Funciones del tema hijo */
-
-add_action( 'wp_enqueue_scripts', 'inmopress_enqueue_styles' );
-function inmopress_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
-
-// Aquí moveremos tu código personalizado.
